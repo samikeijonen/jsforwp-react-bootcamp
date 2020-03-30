d729e9ba8f51ac722e356a965fe94f4b55aa797c
Sami Keijonen <sami.keijonen@foxnet.fi>
2020-03-30 11:27:55 +0300

    Add folder.

---
+++ week-2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Main from './Main';
+import Header from './Header';
+import Footer from './Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ week-2/src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main id="main" className="main">
+      <article>
+        <h2>This is main content of the site</h2>
+        <p>This is some text.</p>
+      </article>
+    </main>
+  );
+}
+
+export default Main;
